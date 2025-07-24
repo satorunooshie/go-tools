4d3c4c7d3cb2c10239ebc3c805477abc124d34b3
Satoru Kitaguchi <rule.the.fate.myfirststory@gmail.com>
2025-07-24 11:12:25 +0900

    fix: remove deprecated enabled field from nosprintf analyzer config

--- golang.org/x/tools/gopls/internal/settings/custom.go
+++ golang.org/x/tools/gopls/internal/settings/custom.go
@@ -9,7 +9,6 @@ func addCustomAnalyzers(a []*Analyzer) []*Analyzer {
 	return append(a, []*Analyzer{
 		{
 			analyzer:    nosprintf.Analyzer,
-			enabled:     true,
 			actionKinds: []protocol.CodeActionKind{protocol.SourceFixAll, protocol.QuickFix},
 		},
 	}...)
